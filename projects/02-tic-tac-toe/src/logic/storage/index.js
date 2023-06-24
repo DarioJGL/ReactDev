1174b36660cb521fc07a1819cc355b2e11dfa40e
DarioJGL <dariojgl0211@gmail.com>
2023-06-24 00:42:18 -0500

    Segundo Proyecto Final

--- projects/02-tic-tac-toe/src/logic/storage/index.js
+++ projects/02-tic-tac-toe/src/logic/storage/index.js
@@ -1,10 +1,10 @@
 export const saveGameToStorage = ({ board, turn }) => {
-    // guardar aqui partida
-    window.localStorage.setItem('board', JSON.stringify(board))
-    window.localStorage.setItem('turn', turn)
-  }
-  
-  export const resetGameStorage = () => {
-    window.localStorage.removeItem('board')
-    window.localStorage.removeItem('turn')
-  }
+  // guardar aqui partida
+  window.localStorage.setItem('board', JSON.stringify(board))
+  window.localStorage.setItem('turn', turn)
+}
+
+export const resetGameStorage = () => {
+  window.localStorage.removeItem('board')
+  window.localStorage.removeItem('turn')
+}
